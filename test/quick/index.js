7085c55145026f4facc0af8c9418dc1e143d6810
Offirmo <offirmo.net@gmail.com>
2016-08-02 18:58:44 +0200

    +

--- test/quick/index.js
+++ test/quick/index.js
@@ -1,71 +1,118 @@
 #!/bin/sh
 ':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../node_modules/.bin/ts-node "$0" "$@"
 
+require('loud-rejection/register')
+const DISPLAY_CODE = false
+
+const originalRequire = require
+require = function(moduleRef) {
+	if (moduleRef.startsWith('@offirmo/cli-toolbox/'))
+		moduleRef = '../../' + moduleRef.slice(21)
+	return originalRequire(moduleRef)
+}
+
 const stylizeString = require('@offirmo/cli-toolbox/output/stylize-string')
 
-function demo(module_name, repo_url, fn) {
-	console.log(`~~~~~~~ ${module_name} ~~~~~~~`)
+
+function demo(moduleName, repoUrl, fn) {
+	console.log(`### ~~~~~~~ ${moduleName} ~~~~~~~`)
+
+	if (DISPLAY_CODE) {
+		console.log('```js')
+		console.log(fn.toString().split('\n').slice(1, -1).map(s => s.slice(2)).join('\n'))
+		console.log('```')
+	}
+
+	console.log('```')
 	return Promise.resolve(fn())
 	.then(
-		() => console.log(stylizeString.dim.italic(`\nSee more at ${stylizeString.blue(repo_url)}`))
+		() => console.log(stylizeString.dim.italic('```' + `\nSee more at ${stylizeString.blue(repoUrl)}`))
 	)
 }
 
 let sequence = Promise.resolve()
 
 ////////////////////////////////////
-sequence = sequence.then(() => demo('output/clear-cli', 'https://github.com/sindresorhus/clear-cli', () => {
-	const clearCli = require('@offirmo/cli-toolbox/output/clear-cli')
+sequence = sequence.then(() => demo(
+	'output/clear-cli',
+	'https://github.com/sindresorhus/clear-cli',
+	() => {
+		const clearCli = require('@offirmo/cli-toolbox/output/clear-cli')
 
-	clearCli()
-	console.log(`~~~~~~~ output/clear-cli ~~~~~~~`)
-}))
-////////////////////////////////////
-sequence = sequence.then(() => demo('output/columnify', 'https://github.com/timoxley/columnify', () => {
-	const columnify = require('@offirmo/cli-toolbox/output/columnify')
-
-	var data = {
-		"commander@0.6.1": 1,
-		"minimatch@0.2.14": 3,
-		"mkdirp@0.3.5": 2,
-		"sigmund@1.0.0": 3
+		clearCli()
+		console.log(`~~~~~~~ output/clear-cli ~~~~~~~`)
 	}
-
-	console.log(columnify(data))
-}))
+))
 ////////////////////////////////////
-sequence = sequence.then(() => demo('output/boxify', 'https://github.com/sindresorhus/boxen', () => {
-	const boxify = require('@offirmo/cli-toolbox/output/boxify')
+sequence = sequence.then(() => demo(
+	'output/columnify',
+	'https://github.com/timoxley/columnify',
+	() => {
+		const columnify = require('@offirmo/cli-toolbox/output/columnify')
 
-	console.log(boxify('Hello'))
-}))
-////////////////////////////////////
-sequence = sequence.then(() => demo('output/prettify-json', 'https://github.com/rafeca/prettyjson', () => {
-	const prettifyJson = require('@offirmo/cli-toolbox/output/prettify-json')
+		var data = {
+			"commander@0.6.1": 1,
+			"minimatch@0.2.14": 3,
+			"mkdirp@0.3.5": 2,
+			"sigmund@1.0.0": 3
+		}
 
-	var data = {
-		username: 'rafeca',
-		url: 'https://github.com/rafeca',
-		twitter_account: 'https://twitter.com/rafeca',
-		projects: ['prettyprint', 'connfu']
+		console.log(columnify(data))
 	}
-
-	var options = {}
-
-	console.log(prettifyJson(data, options))
-}))
+))
 ////////////////////////////////////
-sequence = sequence.then(() => demo('output/stylize-string', 'https://github.com/rafeca/prettyjson', () => {
-	const stylizeString = require('@offirmo/cli-toolbox/output/stylize-string')
+sequence = sequence.then(() => demo(
+	'output/boxify',
+	'https://github.com/sindresorhus/boxen',
+	() => {
+		const boxify = require('@offirmo/cli-toolbox/output/boxify')
 
-	console.log(stylizeString.bold.yellow.bgBlue('Hello'))
-}))
+		console.log(boxify('Hello'))
+	}
+))
 ////////////////////////////////////
-sequence = sequence.then(() => demo('framework/meow', 'https://github.com/sindresorhus/meow', () => {
-	const meow = require('@offirmo/cli-toolbox/framework/meow')
-}))
+sequence = sequence.then(() => demo(
+	'output/prettify-json',
+	'https://github.com/rafeca/prettyjson',
+	() => {
+		const prettifyJson = require('@offirmo/cli-toolbox/output/prettify-json')
+
+		var data = {
+			username: 'rafeca',
+			url: 'https://github.com/rafeca',
+			twitter_account: 'https://twitter.com/rafeca',
+			projects: ['prettyprint', 'connfu']
+		}
+
+		var options = {}
+
+		console.log(prettifyJson(data, options))
+	}
+))
 ////////////////////////////////////
-sequence = sequence.then(() => demo('framework/vorpal', 'https://github.com/dthree/vorpal', () => {
-	const vorpal = require('@offirmo/cli-toolbox/framework/vorpal')
-}))
+sequence = sequence.then(() => demo(
+	'output/stylize-string',
+	'https://github.com/rafeca/prettyjson',
+	() => {
+		const stylizeString = require('@offirmo/cli-toolbox/output/stylize-string')
+
+		console.log(stylizeString.bold.yellow.bgBlue('Hello'))
+	}
+))
+////////////////////////////////////
+sequence = sequence.then(() => demo(
+	'framework/meow',
+	'https://github.com/sindresorhus/meow',
+	() => {
+		const meow = require('@offirmo/cli-toolbox/framework/meow')
+	}
+))
+////////////////////////////////////
+sequence = sequence.then(() => demo(
+	'framework/vorpal',
+	'https://github.com/dthree/vorpal',
+	() => {
+		const vorpal = require('@offirmo/cli-toolbox/framework/vorpal')
+	}
+))
 ////////////////////////////////////
